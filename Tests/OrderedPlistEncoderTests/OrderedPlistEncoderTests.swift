eacf1060eda64fba70cab52250c9e7dce69efd0b
fwcd <fwcdmail@gmail.com>
2023-04-20 18:19:41 +0200

    Add some roundtrip tests

--- Tests/OrderedPlistEncoderTests/OrderedPlistEncoderTests.swift
+++ Tests/OrderedPlistEncoderTests/OrderedPlistEncoderTests.swift
@@ -1,8 +1,28 @@
 import XCTest
 @testable import OrderedPlistEncoder
 
+private struct X: Codable, Hashable {
+    let a: Int
+    let b: String
+    let c: Int
+}
+
 final class OrderedPlistEncoderTests: XCTestCase {
-    func testEncoding() throws {
-        // TODO
+    func testEncoder() throws {
+        try assertRoundtrips(23)
+        try assertRoundtrips(-9)
+        try assertRoundtrips(4.5)
+        try assertRoundtrips("Test this")
+        try assertRoundtrips(" a string with  \nspaces and stuff   ")
+        try assertRoundtrips([1, -9, 3])
+        try assertRoundtrips(["hello": "a", "world": "b"])
+        try assertRoundtrips(X(a: 3, b: "abc", c: -9))
+    }
+
+    private func assertRoundtrips<Value>(_ value: Value, line: UInt = #line) throws where Value: Codable & Equatable {
+        let encoder = OrderedPlistEncoder()
+        let decoder = PropertyListDecoder()
+        let roundtripped = try decoder.decode(Value.self, from: encoder.encode(value))
+        XCTAssertEqual(roundtripped, value, line: line)
     }
 }
