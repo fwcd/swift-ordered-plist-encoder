3cfa08a5842f6d1861be896c307e8f1470aba64d
fwcd <fwcd@fwcd.dev>
2024-10-04 04:12:56 +0200

    Add a simple example

---
+++ Snippets/EncodeValue.swift
@@ -0,0 +1,25 @@
+import OrderedPlistEncoder
+
+struct Planet: Encodable {
+    let name: String
+    let mass: Double
+}
+
+let planets = [
+    // Source: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+    Planet(name: "Mercury", mass: 3.3e23),
+    Planet(name: "Venus", mass: 4.82e24),
+    Planet(name: "Earth", mass: 5.97e24),
+    Planet(name: "Mars", mass: 6.42e23),
+    Planet(name: "Jupiter", mass: 1.898e27),
+    Planet(name: "Saturn", mass: 5.68e26),
+    Planet(name: "Uranus", mass: 8.68e25),
+    Planet(name: "Neptune", mass: 1.02e26),
+    Planet(name: "Pluto", mass: 1.3e22),
+]
+
+let encoder = OrderedPlistEncoder(options: [
+    .prettyPrinted,
+])
+
+print(try encoder.encodeToString(planets))
